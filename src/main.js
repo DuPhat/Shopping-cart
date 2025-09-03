6acd027274365597f63bf783b5c099117f0af30e
DuPhat <duphat2003@gmail.com>
2025-09-03 20:14:23 +0700

    Add files

--- src/main.js
+++ src/main.js
@@ -1,103 +1,45 @@
 let shop = document.getElementById("shop");
 
-/**
- * ! Giỏ hàng để lưu tất cả sản phẩm đã chọn
- * ? getItem là lấy dữ liệu từ localStorage
- * ? nếu localStorage trống, thì basket trở thành mảng rỗng
- */
-
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-
-/**
- * ! Sinh ra cửa hàng với các thẻ sản phẩm gồm:
- * ! hình ảnh, tên, giá, nút bấm, mô tả
- */
 
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, desc, img, price } = x;
-      let search = basket.find((y) => y.id === id) || [];
       return `
-    <div id=product-id-${id} class="item">
-      <img width="220" src=${img} alt="">
-      <div class="details">
-        <h3>${name}</h3>
-        <p>${desc}</p>
-        <div class="price-quantity">
-          <h2>${price.toLocaleString("vi-VN")} ₫</h2>
-          <div class="buttons">
-            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-            <div id=${id} class="quantity">${
-        search.item === undefined ? 0 : search.item
-      }</div>
-            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+      <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>${price.toLocaleString("vi-VN")} ₫</h2>
+            <button onclick="addToCart('${id}')" class="add-btn">Thêm vào giỏ hàng</button>
           </div>
         </div>
       </div>
-  </div>
-    `;
+      `;
     })
     .join(""));
 };
 
 generateShop();
 
-/**
- * ! Dùng để tăng số lượng sản phẩm đã chọn lên 1
- */
-
-let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+let addToCart = (id) => {
+  let search = basket.find((x) => x.id === id);
 
   if (search === undefined) {
     basket.push({
-      id: selectedItem.id,
+      id: id,
       item: 1,
     });
   } else {
     search.item += 1;
   }
 
-  console.log(basket);
-  update(selectedItem.id);
   localStorage.setItem("data", JSON.stringify(basket));
-};
-
-/**
- * ! Dùng để giảm số lượng sản phẩm đã chọn đi 1
- */
-
-let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
-
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-
-  update(selectedItem.id);
-  basket = basket.filter((x) => x.item !== 0);
-  console.log(basket);
-  localStorage.setItem("data", JSON.stringify(basket));
-};
-
-/**
- * ! Cập nhật số lượng hiển thị của từng sản phẩm trong thẻ sản phẩm
- */
-
-let update = (id) => {
-  let search = basket.find((x) => x.id === id);
-  document.getElementById(id).innerHTML = search.item;
   calculation();
 };
-
-/**
- * ! Tính tổng số sản phẩm đã chọn
- */
 
 let calculation = () => {
   let cartIcon = document.getElementById("cartAmount");
