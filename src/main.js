0d2820e001e5343f79e327c454c0303e1575131f
DuPhat <duphat2003@gmail.com>
2025-09-03 20:19:18 +0700

    Add files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+/**
+ * ! Lấy phần tử shop trong HTML để hiển thị danh sách sản phẩm
+ */
 let shop = document.getElementById("shop");
 
+/**
+ * ! Giỏ hàng để lưu tất cả sản phẩm đã chọn
+ * ? Lấy dữ liệu giỏ hàng từ localStorage
+ * ? Nếu localStorage trống thì basket sẽ là mảng rỗng
+ */
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
+/**
+ * ! Hàm generateShop: Sinh giao diện danh sách sản phẩm
+ * ? Duyệt qua shopItemsData để render từng sản phẩm
+ * ? Hiển thị hình ảnh, tên, mô tả, giá và nút "Thêm vào giỏ hàng"
+ */
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
@@ -25,6 +38,12 @@ let generateShop = () => {
 
 generateShop();
 
+/**
+ * ! Hàm addToCart: Xử lý khi bấm nút "Thêm vào giỏ hàng"
+ * ? Nếu sản phẩm chưa có trong basket thì thêm mới với item = 1
+ * ? Nếu đã có thì tăng số lượng item lên 1
+ * ? Sau đó lưu lại basket vào localStorage
+ */
 let addToCart = (id) => {
   let search = basket.find((x) => x.id === id);
 
@@ -41,6 +60,11 @@ let addToCart = (id) => {
   calculation();
 };
 
+/**
+ * ! Hàm calculation: Cập nhật tổng số sản phẩm trong giỏ
+ * ? Lấy ra tất cả số lượng item trong basket
+ * ? Tính tổng và hiển thị ở icon giỏ hàng (cartAmount)
+ */
 let calculation = () => {
   let cartIcon = document.getElementById("cartAmount");
   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
